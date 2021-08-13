e869b99e4860df9ecc358d7e491dbbbe94fffb75
andreybannikov <andrewnnov@yandex.ru>
2021-08-13 21:18:05 +0300

    about

--- tests/0_pytest_intro/test_pytest_intro.py
+++ tests/0_pytest_intro/test_pytest_intro.py
@@ -5,8 +5,8 @@ def test_one():
 
 
 # Так тоже можно, но не нужно и с модулем не заработает
-def testwo():
-    assert "Here" == "Where?"
+def tes_two():
+    assert "Here" == "Here"
 
 
 # Создание тестовых классов
@@ -17,5 +17,5 @@ class TestClass:
         list_to_test2 = []
         assert list_to_test1 is list_to_test2
 
-    def testtwo(self):
+    def test_two(self):
         assert 1 is 1
